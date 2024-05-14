27b13ea19cdc4d5403056f644fab2e2bd5fa8d83
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-05-14 15:51:21 +0800

    Lab 7 Page 3

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -20,28 +20,47 @@ class _TasksScreenState extends State<TasksScreen> {
     return Scaffold(
       backgroundColor: Colors.cyan,
       body: SafeArea(
-        child: Column(
-          children: [
-            const CircleAvatar(
-              backgroundColor: Colors.blue,
-              child: Icon(Icons.list),
-            ),
-            const SizedBox(height: 10.0),
-            const Text('Tasks Tracker'),
-            Text('${tasks.length} Tasks'),
-            const SizedBox(height: 10.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(tasks[index].title),
-                    subtitle: Text(tasks[index].module),
-                  );
-                },
+        child: Padding(
+          padding: const EdgeInsets.only(
+              top: 40.0, left: 15.0, right: 15.0, bottom: 0),
+          child: Column(
+            children: [
+              const SizedBox(height: 10.0),
+              const CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.list),
               ),
-            ),
-          ],
+              const SizedBox(height: 10.0),
+              const Text('Tasks Tracker'),
+              Text('${tasks.length} Tasks'),
+              const SizedBox(height: 10.0),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: ListView.builder(
+                    itemCount: tasks.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(tasks[index].title),
+                        subtitle: Text(tasks[index].module),
+                        trailing: Checkbox(
+                          value: tasks[index].isDone,
+                          onChanged: (value) {},
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
